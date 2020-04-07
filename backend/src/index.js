086cf04b4c503db2f599721194523b6c839a2d8f
gustavocastrow <gustavocastrocs@gmail.com>
2020-04-07 17:54:09 -0300

    Configurando o nodemon

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,9 +5,12 @@ const app = express();
 
 //ROTA
 app.get("/", (request, response) => {
-  return response.json({ message: "Hello Gostack", date: "07-04-2020"});
+  return response.json({ message: "Hello Gostack <3", date: "07-04-2020"});
 
 })
 
 
-app.listen(3333);
+app.listen(3333, () => {
+  console.log('🚀 Back-end started!');
+
+});
