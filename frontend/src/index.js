cce5db36e3b9ee72336d0cc751163c8ebe82e8d2
gustavocastrow <gustavocastrocs@gmail.com>
2020-04-10 20:16:40 -0300

    Components

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,2 +1,7 @@
-import { soma } from './soma';
-console.log(soma(3,5));
+import React from 'react';
+import { render } from 'react-dom';
+import App from './App';
+
+//JSX: HTML dentro do Javascript (Javascript XML)
+
+render(<App />, document.getElementById('app'));
